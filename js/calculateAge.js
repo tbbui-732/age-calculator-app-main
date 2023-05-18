0cf160e490ae12debdc169f3aa0efcffe4211eb5
Thien-Bao Bui <80117447+tbbui-732@users.noreply.github.com>
2023-05-18 19:49:46 -0400

    Deleted TS files and rewrote JS files

--- js/calculateAge.js
+++ js/calculateAge.js
@@ -1,75 +1,77 @@
-"use strict";
-exports.__esModule = true;
-exports.calculateAge = void 0;
-// GLOBAL VALUES
-// Storing current date of program
-var dayInMilliseconds = 1000 * 60 * 60 * 24;
-var currDate = new Date();
-var currDay = currDate.getDate();
-var currMonth = currDate.getMonth() + 1;
-var currYear = currDate.getFullYear();
-// Reformat input to 'MM/DD/YYYY'
-function _reformatDate(day, month, year) {
-    var newDay;
-    var newMonth;
-    var newYear;
-    if (day < 10)
-        newDay = '0' + String(day);
-    else
-        newDay = String(day);
-    if (month < 10)
-        newMonth = '0' + String(month);
-    else
-        newMonth = String(month);
-    newYear = String(year);
-    return newMonth + '/' + newDay + '/' + newYear;
+// global variables
+const dayInMilliSeconds = 1000*60*60*24;
+let currDate = new Date();
+let currDay = currDate.getDate();
+let currMonth = currDate.getMonth() + 1;
+let currYear = currDate.getFullYear();
+
+// reformat input to 'mm/dd/yyyy'
+function reformatDate(day, month, year) {
+  day = String(day);
+  month = String(month);
+  year = String(year);
+
+  if (day < 10) 
+    day = '0' + day;
+
+  if (month < 10)
+    month = '0' + month;
+
+  return month + '/' + day + '/' + year;
 }
-// Get number of days between birthday and current day
-function _getDateDifferenceInDays(userDay, userMonth, userYear) {
-    // Reformat number to string
-    var bday = _reformatDate(userDay, userMonth, userYear);
-    var today = _reformatDate(currDay, currMonth, currYear);
-    // Format string to Date
-    var startDate = new Date(bday);
-    var endDate = new Date(today);
-    // Convert time in milliseconds to days
-    var timeDifference = startDate.getTime() - endDate.getTime(); // given in milliseconds
-    var totalDays = Math.floor(Math.abs(timeDifference / dayInMilliseconds));
-    return totalDays;
+
+// get number of days between DOB and the current date
+function getDateDifferenceInDays(userDay, userMonth, userYear) {
+  // reformat number to strings
+  let bday = reformatDate(userDay, userMonth, userYear);
+  let today = reformatDate(currDay, currMonth, currYear);
+
+  // format string to 'Date' type
+  let startDate = new Date(bday);
+  let endDate = new Date(today);
+
+  // convert difference in milliseconds to days
+  let timeDifference = startDate.getTime() - endDate.getTime();
+  let totalDays = Math.floor(Math.abs(timeDifference / dayInMilliSeconds));
+  return totalDays;
 }
-function _isLeapYear(year) {
-    return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+
+// checks if a year is a leap year
+function isLeapYear(year) {
+  return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
 }
-// Converts total number of days to [num_year, num_months, num_days]
-function _convertDays(days) {
-    var daysPerMonth = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-    var years = days; // 365.25;
-    days %= 365.25;
-    var year = 1;
-    while (1) {
-        var leapYear = _isLeapYear(year);
-        if (leapYear)
-            daysPerMonth[2] = 29;
-        else
-            daysPerMonth[2] = 28;
-        for (var i = 1; i < 13; i++) {
-            if (days >= daysPerMonth[i])
-                days -= daysPerMonth[i];
-            else
-                return [years, i - 1, days];
-        }
-        year += 1;
+
+// converts total number of days to an arr
+//  consisting of number of year, months, and days
+function convertDays(days) {
+  let daysPerMonth = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+  let years = days // 365.25;
+  days %= 365.25;
+
+  let year = 1;
+  while (1) {
+    let leapYear = isLeapYear(year);
+    daysPerMonth[2] = leapYear ? 29 : 28
+
+    for (let i = 1; i < 13; i++) {
+      if (days >= daysPerMonth[i])
+        days -= daysPerMonth[i];
+      else
+        return [years, i-1, days];
+
     }
-    return [-1, -1, -1];
+    year += 1;
+  }
 }
-// PUBLIC METHOD
-// Returns the date as [YYYY, MM, DD] Array<number>
-function calculateAge() {
-    var userDay = parseInt(document.getElementById("day").value);
-    var userMonth = parseInt(document.getElementById("month").value);
-    var userYear = parseInt(document.getElementById("year").value);
-    var numDays = _getDateDifferenceInDays(userDay, userMonth, userYear);
-    var dateArray = _convertDays(numDays);
-    return dateArray;
+
+// -- PUBLIC METHOD --
+export function calculateAge() {
+  let userDay = parseInt(document.getElementById("day").value);
+  let userMonth = parseInt(document.getElementById("month").value);
+  let userYear = parseInt(document.getElementById("year").value);
+
+  let totalDays = getDateDifferenceInDays(userDay, userMonth, userYear); 
+  let dateArray = convertDays(totalDays);
+
+  return dateArray;
 }
-exports.calculateAge = calculateAge;
